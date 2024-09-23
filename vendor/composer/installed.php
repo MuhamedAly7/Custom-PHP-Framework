8f70a5aebb07cecaeb24cbdd3da01eedddd7cea5
MuhamedAly7 <muhammedaly021@gmail.com>
2024-09-23 18:22:37 +0300

    prepare our setup

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0',
+        'version' => '1.0.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'mali/customframework',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mali/customframework' => array(
+            'pretty_version' => '1.0',
+            'version' => '1.0.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'mvc/customframework' => array(
+            'pretty_version' => '1.0',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mvc/customframework',
+            'aliases' => array(),
+            'reference' => '27e7fd7bc28a105ab16f01102898d3bdf800362c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
